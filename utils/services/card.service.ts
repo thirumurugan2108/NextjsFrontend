46dafbe5bc90de6df6c62b0936d5b486042d5328
thirumurugan2108 <thirumurugan2108@gmail.com>
2022-02-02 14:10:03 +0530

    url changed

--- utils/services/card.service.ts
+++ utils/services/card.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseUrl = 'http://ec2-13-126-51-246.ap-south-1.compute.amazonaws.com:3001/v1/auth/';
-const baseUrl = 'http://localhost:3001/v1/cards/';
+const baseUrl = 'http://bingemeee.com:3001/v1/cards/';
 
 export function getcardDetails() {
   return axios.get(baseUrl,
