67b887a8a62f8c1cde3bb55eadaf2959514972a2
thirumurugan2108 <thirumurugan2108@gmail.com>
2022-02-08 15:25:12 +0530

    url modified

--- utils/services/card.service.ts
+++ utils/services/card.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://api.bingemeee.com/v1/auth/';
+const baseUrl = 'https://api.bingemeee.com/v1/cards/';
 // const baseUrl = 'http://localhost:3001/v1/cards/';
 
 export function getcardDetails() {
