b6316023a3652346d82aae079a9ba729ef28e3d6
vijaykarthick <p.vijaykarthick@gmail.com>
2022-05-06 19:54:54 +0530

    fixing issue

--- utils/sharedComponents/uploadBox/uploadBox.jsx
+++ utils/sharedComponents/uploadBox/uploadBox.jsx
@@ -3,18 +3,18 @@ import classes from "./upladBox.module.scss";
 
 export const siteTitle = "Next.js Sample Website";
 
-export default function UploadBox(props: any) {
+export default function UploadBox(props) {
   const [fileNames, setfileNames] = useState([]);
-  async function onFileChange(event: any) {
+  async function onFileChange(event) {
     // setSelectedFile(event.target.files[0]);
     // if (event?.target?.files.length >5) {
     //   alert("You can able to upload upto 5 images only.")
     // }
     // else {
-      const flnames = []
+      let flnames = []
       if (event?.target?.files) {
         console.log(event?.target?.files)
-        Array.from(event.target.files).map((fl:any)=> {
+        Array.from(event.target.files).map((fl)=> {
           flnames.push(fl.name)
         })
         setfileNames(flnames);
